abf8d74692b8e385f448da6d99120ae27af2e658
unknown <lloyd.aidan@gmail.com>
2013-03-04 22:19:28 -0500

    add -reverse flag

--- starting_holdem_hands.cpp
+++ starting_holdem_hands.cpp
@@ -7,7 +7,16 @@ using namespace std;
 #define MAIN_MODULE
 #include "poker_hand.h"
 
+static char usage[] = "usage: starting_holdem_hands (-indexes) (-reverse)\n";
+static char fmt[] = "%s %s\n";
+
 static void get_permutation_instance(
+  int set_size,int subset_size,
+  int *m,int *n,
+  int instance_ix
+);
+
+static void get_permutation_instance_reverse(
   int set_size,int subset_size,
   int *m,int *n,
   int instance_ix
@@ -15,29 +24,54 @@ static void get_permutation_instance(
 
 int main(int argc,char **argv)
 {
+  int curr_arg;
   int m;
   int n;
   int o;
-  char card_string[3];
+  char card_string[2][3];
   bool bIndexes;
+  bool bReverse;
 
   bIndexes = false;
+  bReverse = false;
 
-  if ((argc == 2) && !strcmp(argv[1],"-indexes"))
-    bIndexes = true;
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-indexes"))
+      bIndexes = true;
+    else if (!strcmp(argv[curr_arg],"-reverse"))
+      bReverse = true;
+    else
+      break;
+  }
 
-  card_string[2] = 0;
+  if (argc != curr_arg) {
+    printf(usage);
+    return 1;
+  }
+
+  for (n = 0; n < 2; n++)
+    card_string[n][2] = 0;
 
   for (o = 0; o < POKER_52_2_PERMUTATIONS; o++) {
-    get_permutation_instance(
-      NUM_CARDS_IN_DECK,NUM_HOLE_CARDS_IN_HOLDEM_HAND,
-      &m,&n,o);
+    if (!bReverse) {
+      get_permutation_instance(
+        NUM_CARDS_IN_DECK,NUM_HOLE_CARDS_IN_HOLDEM_HAND,
+        &m,&n,o);
+    }
+    else {
+      get_permutation_instance_reverse(
+        NUM_CARDS_IN_DECK,NUM_HOLE_CARDS_IN_HOLDEM_HAND,
+        &m,&n,o);
+    }
 
     if (!bIndexes) {
-      card_string_from_card_value(m,card_string);
-      printf("%s ",card_string);
-      card_string_from_card_value(n,card_string);
-      printf("%s\n",card_string);
+      card_string_from_card_value(m,card_string[0]);
+      card_string_from_card_value(n,card_string[1]);
+
+      if (rank_of(m) >= rank_of(n))
+        printf(fmt,card_string[0],card_string[1]);
+      else
+        printf(fmt,card_string[1],card_string[0]);
     }
     else
       printf("%02d %02d\n",m,n);
@@ -55,8 +89,27 @@ static void get_permutation_instance(
   if (instance_ix)
     goto after_return_point;
 
-  for (*m = 0; *m < set_size - subset_size + 1; (*m)++) {
-    for (*n = *m + 1; *n < set_size - subset_size + 2; (*n)++) {
+  for (*m = 0; *m < set_size - 1; (*m)++) {
+    for (*n = *m + 1; *n < set_size; (*n)++) {
+      return;
+
+      after_return_point:
+      ;
+    }
+  }
+}
+
+static void get_permutation_instance_reverse(
+  int set_size,int subset_size,
+  int *m,int *n,
+  int instance_ix
+)
+{
+  if (instance_ix)
+    goto after_return_point;
+
+  for (*m = set_size - 1; *m > 0; (*m)--) {
+    for (*n = *m - 1; *n >= 0; (*n)--) {
       return;
 
       after_return_point:
