284da70ef1de9ca96a5ff51f212737b8b2bf0f47
unknown <lloyd.aidan@gmail.com>
2013-07-04 18:52:24 -0400

    calculate computation time

--- four_turn.cpp
+++ four_turn.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 using namespace std;
 
 #define MAIN_MODULE
@@ -47,6 +48,8 @@ int main(int argc,char **argv)
   double pct;
   int work_wins;
   int work_losses;
+  time_t start_time;
+  time_t end_time;
 
   if ((argc < 2) || (argc > 3)) {
     cout << usage << endl;
@@ -71,6 +74,8 @@ int main(int argc,char **argv)
     printf(couldnt_open,argv[curr_arg]);
     return 3;
   }
+
+  time(&start_time);
 
   line_no = 0;
 
@@ -207,6 +212,10 @@ int main(int argc,char **argv)
   }
 
   fclose(fptr);
+
+  time(&end_time);
+
+  printf("\ncomputation time: %d seconds\n",end_time - start_time);
 
   return 0;
 }
