ae946d535a277f1868b2385a0962ed094379c2b8
unknown <lloyd.aidan@gmail.com>
2013-08-01 15:11:45 -0400

    add fflopped_hand.cpp

---
+++ fflopped_hand.cpp
@@ -0,0 +1,363 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#ifdef WIN32
+#include <direct.h>
+#else
+#define _MAX_PATH 4096
+#include <unistd.h>
+#endif
+using namespace std;
+
+#define MAIN_MODULE
+#include "poker_hand.h"
+
+#define MAX_FILENAME_LEN 1024
+static char filename[MAX_FILENAME_LEN];
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static char usage[] =
+"usage: fflopped_hand (-terse) (-verbose) (-debug) (-hand_typehand_type)\n"
+"  player_name filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+
+static char in_chips[] = " in chips";
+#define IN_CHIPS_LEN (sizeof (in_chips) - 1)
+static char flop[] = "*** FLOP *** [";
+#define FLOP_LEN (sizeof (flop) - 1)
+static char dealt_to[] = "Dealt to ";
+#define DEALT_TO_LEN (sizeof (dealt_to) - 1)
+static char folds[] = " folds ";
+#define FOLDS_LEN (sizeof (folds) - 1)
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+static int Contains(bool bCaseSens,char *line,int line_len,
+  char *string,int string_len,int *index);
+static int get_work_amount(char *line,int line_len);
+
+int main(int argc,char **argv)
+{
+  int m;
+  int n;
+  int p;
+  int q;
+  int curr_arg;
+  bool bTerse;
+  bool bVerbose;
+  bool bDebug;
+  int player_name_ix;
+  int player_name_len;
+  FILE *fptr0;
+  int filename_len;
+  FILE *fptr;
+  int line_len;
+  int line_no;
+  int dbg_line_no;
+  int ix;
+  int end_ix;
+  int file_no;
+  int dbg_file_no;
+  int num_hands;
+  int dbg;
+  int work;
+  double dwork1;
+  double dwork2;
+  char hole_cards[12];
+  char board_cards[15];
+  bool bHandTypeSpecified;
+  char *hand_type;
+  char *hand;
+  bool bSkipping;
+  int rank_ix1;
+  int rank_ix2;
+  int cards[NUM_CARDS_IN_HOLDEM_POOL];
+  PokerHand poker_hand;
+  HoldemPokerHand holdem_hand;
+  char card_string[3];
+  int retval;
+
+  if ((argc < 3) || (argc > 66)) {
+    printf(usage);
+    return 1;
+  }
+
+  bTerse = false;
+  bVerbose = false;
+  bDebug = false;
+  bHandTypeSpecified = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-terse"))
+      bTerse = true;
+    else if (!strcmp(argv[curr_arg],"-verbose"))
+      bVerbose = true;
+    else if (!strcmp(argv[curr_arg],"-debug"))
+      bDebug = true;
+    else if (!strncmp(argv[curr_arg],"-hand_type",10)) {
+      hand_type = &argv[curr_arg][10];
+      bHandTypeSpecified = true;
+    }
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 2) {
+    printf(usage);
+    return 2;
+  }
+
+  if (bTerse && bVerbose) {
+    printf("can't specify both -terse and -verbose\n");
+    return 3;
+  }
+
+  player_name_ix = curr_arg++;
+  player_name_len = strlen(argv[player_name_ix]);
+
+  if ((fptr0 = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 4;
+  }
+
+  file_no = 0;
+  dbg_file_no = -1;
+
+  hole_cards[11] = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr0,filename,&filename_len,MAX_FILENAME_LEN);
+
+    if (feof(fptr0))
+      break;
+
+    file_no++;
+
+    if (dbg_file_no == file_no)
+      dbg = 1;
+
+    if ((fptr = fopen(filename,"r")) == NULL) {
+      printf(couldnt_open,filename);
+      continue;
+    }
+
+    line_no = 0;
+    bSkipping = false;
+    num_hands = 0;
+
+    for ( ; ; ) {
+      GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+      if (feof(fptr))
+        break;
+
+      line_no++;
+
+      if (line_no == dbg_line_no)
+        dbg = 1;
+
+      if (bDebug)
+        printf("line %d %s\n",line_no,line);
+
+      if (Contains(true,
+        line,line_len,
+        argv[player_name_ix],player_name_len,
+        &ix)) {
+
+        if (Contains(true,
+          line,line_len,
+          in_chips,IN_CHIPS_LEN,
+          &ix)) {
+
+          num_hands++;
+          bSkipping = false;
+
+          continue;
+        }
+        else if (bSkipping)
+          continue;
+        else if (!strncmp(line,dealt_to,DEALT_TO_LEN)) {
+          for (n = 0; n < line_len; n++) {
+            if (line[n] == '[')
+              break;
+          }
+
+          if (n < line_len) {
+            n++;
+
+            for (m = n; m < line_len; m++) {
+              if (line[m] == ']')
+                break;
+            }
+
+            if (m < line_len) {
+              for (p = 0; p < 11; p++) {
+                if (line[n+p] == ']') {
+                  if (p == 5)
+                    hole_cards[p] = 0;
+
+                  break;
+                }
+                else
+                  hole_cards[p] = line[n+p];
+              }
+
+              for (q = 0; q < NUM_HOLE_CARDS_IN_HOLDEM_HAND; q++) {
+                card_string[0] = hole_cards[q*3 + 0];
+                card_string[1] = hole_cards[q*3 + 1];
+
+                retval = card_value_from_card_string(
+                  card_string,&cards[q]);
+
+                if (retval) {
+                  printf("invalid card string %s on line %d\n",
+                    card_string,line_no);
+                  return 5;
+                }
+              }
+            }
+          }
+        }
+        else if (Contains(true,
+          line,line_len,
+          folds,FOLDS_LEN,
+          &ix)) {
+
+          bSkipping = true;
+
+          continue;
+        }
+      }
+      else if (!bSkipping) {
+        if (!strncmp(line,flop,FLOP_LEN)) {
+          for (n = 0; n < 8; n++)
+            board_cards[n] = line[FLOP_LEN+n];
+
+          board_cards[n] = ' ';
+
+          for (q = 0; q < NUM_CARDS_IN_FLOP; q++) {
+            card_string[0] = board_cards[q*3 + 0];
+            card_string[1] = board_cards[q*3 + 1];
+
+            retval = card_value_from_card_string(
+              card_string,&cards[NUM_HOLE_CARDS_IN_HOLDEM_HAND+q]);
+
+            if (retval) {
+              printf("invalid card string %s on line %d\n",
+                card_string,line_no);
+              return 6;
+            }
+          }
+
+          poker_hand.NewCards(cards[0],cards[1],cards[2],cards[3],cards[4]);
+          poker_hand.Evaluate();
+          printf("%s",hole_cards);
+          printf(" %s",plain_hand_types[poker_hand.GetHandType()]);
+
+          if (bVerbose)
+            printf(" %s %3d\n",filename,num_hands);
+          else
+            putchar(0x0a);
+        }
+      }
+    }
+
+    fclose(fptr);
+  }
+
+  fclose(fptr0);
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
+
+static int Contains(bool bCaseSens,char *line,int line_len,
+  char *string,int string_len,int *index)
+{
+  int m;
+  int n;
+  int tries;
+  char chara;
+
+  tries = line_len - string_len + 1;
+
+  if (tries <= 0)
+    return false;
+
+  for (m = 0; m < tries; m++) {
+    for (n = 0; n < string_len; n++) {
+      chara = line[m + n];
+
+      if (!bCaseSens) {
+        if ((chara >= 'A') && (chara <= 'Z'))
+          chara += 'a' - 'A';
+      }
+
+      if (chara != string[n])
+        break;
+    }
+
+    if (n == string_len) {
+      *index = m;
+      return true;
+    }
+  }
+
+  return false;
+}
+
+static int get_work_amount(char *line,int line_len)
+{
+  int ix;
+  int chara;
+  int work_amount;
+
+  work_amount = 0;
+
+  for (ix = line_len - 1; (ix >= 0); ix--) {
+    chara = line[ix];
+
+    if ((chara >= '0') && (chara <= '9'))
+      break;
+  }
+
+  if (ix + 1 != line_len);
+    line[ix + 1] = 0;
+
+  for ( ; (ix >= 0); ix--) {
+    chara = line[ix];
+
+    if ((chara < '0') || (chara > '9'))
+      break;
+  }
+
+  sscanf(&line[ix+1],"%d",&work_amount);
+
+
+  return work_amount;
+}
