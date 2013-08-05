400e1eec512999104b9636fcc3246b1ba2d3f3f3
unknown <lloyd.aidan@gmail.com>
2013-08-04 21:02:58 -0400

    add info for players 2 and 3, including hand counts

--- three_flop.cpp
+++ three_flop.cpp
@@ -28,6 +28,9 @@ int main(int argc,char **argv)
   int n;
   int o;
   int p;
+  int s;
+  int t;
+  int u;
   int retval;
   FILE *fptr;
   int line_no;
@@ -36,14 +39,10 @@ int main(int argc,char **argv)
   int remaining_cards[NUM_REMAINING_CARDS];
   HoldemPokerHand holdem_hand[NUM_PLAYERS];
   PokerHand hand[NUM_PLAYERS];
-  int ret_compare;
-  int wins;
-  int losses;
-  int ties;
+  struct outcomes outcomes[NUM_PLAYERS];
   int total;
+  int comparisons[POKER_3_2_PERMUTATIONS];
   double pct;
-  int work_wins;
-  int work_losses;
   time_t start_time;
   time_t end_time;
 
@@ -136,10 +135,19 @@ int main(int argc,char **argv)
         remaining_cards[m++] = n;
     }
 
-    wins = 0;
-    losses = 0;
-    ties = 0;
-    total = 0;
+    for (n = 0; n < NUM_PLAYERS; n++) {
+      outcomes[n].wins = 0;
+      outcomes[n].losses = 0;
+      outcomes[n].ties = 0;
+
+      if (bDebug) {
+        for (m = 0; m < NUM_HAND_TYPES; m++) {
+          outcomes[n].wins_hand_counts[m] = 0;
+          outcomes[n].losses_hand_counts[m] = 0;
+          outcomes[n].ties_hand_counts[m] = 0;
+        }
+      }
+    }
 
     for (o = 0; o < POKER_43_2_PERMUTATIONS; o++) {
       get_permutation_instance_two(
@@ -161,50 +169,135 @@ int main(int argc,char **argv)
       for (p = 0; p < NUM_PLAYERS; p++)
         hand[p] = holdem_hand[p].BestPokerHand();
 
-      work_wins = 0;
-      work_losses = 0;
+      for (u = 0; u < POKER_3_2_PERMUTATIONS; u++) {
+        get_permutation_instance_two(
+          NUM_PLAYERS,
+          &s,&t,u);
 
-      for (p = 0; p < NUM_PLAYERS - 1; p++) {
-        ret_compare = hand[0].Compare(hand[1+p],0);
-
-        if (ret_compare == 1)
-          work_wins++;
-        else if (ret_compare == -1)
-          work_losses++;
+        comparisons[u] = hand[s].Compare(hand[t],0);
       }
 
-      if (work_wins == NUM_PLAYERS - 1)
-        wins++;
-      else if (work_losses)
-        losses++;
-      else
-        ties++;
+      // player 1
 
-      total++;
+      if ((comparisons[0] == 1) && (comparisons[1] == 1)) {
+        outcomes[0].wins++;
+
+        if (bDebug)
+          outcomes[0].wins_hand_counts[hand[0].GetHandType()]++;
+      }
+      else if ((comparisons[0] == -1) || (comparisons[1] == -1)) {
+        outcomes[0].losses++;
+
+        if (bDebug)
+          outcomes[0].losses_hand_counts[hand[0].GetHandType()]++;
+      }
+      else {
+        outcomes[0].ties++;
+
+        if (bDebug)
+          outcomes[0].ties_hand_counts[hand[0].GetHandType()]++;
+      }
+
+      // player 2
+
+      if ((comparisons[0] == -1) && (comparisons[2] == 1)) {
+        outcomes[1].wins++;
+
+        if (bDebug)
+          outcomes[1].wins_hand_counts[hand[1].GetHandType()]++;
+      }
+      else if ((comparisons[0] == 1) || (comparisons[2] == -1)) {
+        outcomes[1].losses++;
+
+        if (bDebug)
+          outcomes[1].losses_hand_counts[hand[1].GetHandType()]++;
+      }
+      else {
+        outcomes[1].ties++;
+
+        if (bDebug)
+          outcomes[1].ties_hand_counts[hand[1].GetHandType()]++;
+      }
+
+      // player 3
+
+      if ((comparisons[1] == -1) && (comparisons[2] == -1)) {
+        outcomes[2].wins++;
+
+        if (bDebug)
+          outcomes[2].wins_hand_counts[hand[2].GetHandType()]++;
+      }
+      else if ((comparisons[1] == 1) || (comparisons[2] == 1)) {
+        outcomes[2].losses++;
+
+        if (bDebug)
+          outcomes[2].losses_hand_counts[hand[2].GetHandType()]++;
+      }
+      else {
+        outcomes[2].ties++;
+
+        if (bDebug)
+          outcomes[2].ties_hand_counts[hand[2].GetHandType()]++;
+      }
     }
 
-    pct = (double)wins * (double)100 / (double)total;
-    printf("  wins      %7d (%5.2lf)\n",wins,pct);
+    putchar(0x0a);
 
-    pct = (double)losses * (double)100 / (double)total;
-    printf("  losses    %7d (%5.2lf)\n",losses,pct);
+    for (n = 0; n < NUM_PLAYERS; n++) {
+      printf("player %d\n",n+1);
+      total = outcomes[n].wins + outcomes[n].losses + outcomes[n].ties;
 
-    if (ties) {
-      pct = (double)ties * (double)100 / (double)total;
-      printf("  ties      %7d (%5.2lf)\n",ties,pct);
+      pct = (double)outcomes[n].wins * (double)100 / (double)total;
+      printf("  wins      %7d (%5.2lf)\n",outcomes[n].wins,pct);
+
+      if (bDebug) {
+        for (m = 0; m < NUM_HAND_TYPES; m++) {
+          if (outcomes[n].wins_hand_counts[m]) {
+            printf("    %s      %7d\n",
+              hand_type_abbrevs[m],
+              outcomes[n].wins_hand_counts[m]);
+          }
+        }
+      }
+
+      pct = (double)outcomes[n].losses * (double)100 / (double)total;
+      printf("  losses    %7d (%5.2lf)\n",outcomes[n].losses,pct);
+
+      if (bDebug) {
+        for (m = 0; m < NUM_HAND_TYPES; m++) {
+          if (outcomes[n].losses_hand_counts[m]) {
+            printf("    %s      %7d\n",
+              hand_type_abbrevs[m],
+              outcomes[n].losses_hand_counts[m]);
+          }
+        }
+      }
+
+      pct = (double)outcomes[n].ties * (double)100 / (double)total;
+      printf("  ties      %7d (%5.2lf)\n",outcomes[n].ties,pct);
+
+      if (bDebug) {
+        for (m = 0; m < NUM_HAND_TYPES; m++) {
+          if (outcomes[n].ties_hand_counts[m]) {
+            printf("    %s      %7d\n",
+              hand_type_abbrevs[m],
+              outcomes[n].ties_hand_counts[m]);
+          }
+        }
+      }
+
+      printf("  total     %7d\n",total);
     }
-
-    printf("  total     %7d\n",total);
 
     if (bDebug) {
-      printf("  num_evaluations        %10d\n",num_evaluations);
-      printf("  num_unique_evaluations %10d\n",num_unique_evaluations);
-      printf("  num_comparisons        %10d\n",num_comparisons);
+      putchar(0x0a);
+      printf("num_evaluations        %10d\n",num_evaluations);
+      printf("num_unique_evaluations %10d\n",num_unique_evaluations);
+      printf("num_comparisons        %10d\n",num_comparisons);
     }
   }
 
   fclose(fptr);
-
   time(&end_time);
 
   printf("\ncomputation time: %d seconds\n",end_time - start_time);
