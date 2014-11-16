eaff45e1b801bb778a3fc46ffaca2ebabe4cdfd0
Aidan Lloyd <lloyd.aidan@gmail.com>
2014-11-16 18:45:35 -0500

    add -dont_ignore_order flag

--- starting_holdem_hands.cpp
+++ starting_holdem_hands.cpp
@@ -8,7 +8,7 @@ using namespace std;
 #include "poker_hand.h"
 
 static char usage[] =
-"usage: starting_holdem_hands (-indexes) (-with_zero_deltas\n";
+"usage: starting_holdem_hands (-indexes) (-with_zero_deltas) (-dont_ignore_order)\n";
 static char *fmt[] = {
   "%s %s\n",
   "         0 %s %s\n",
@@ -23,15 +23,19 @@ int main(int argc,char **argv)
   char card_string[2][3];
   bool bIndexes;
   bool bWithZeroDeltas;
+  bool bDontIgnoreOrder;
 
   bIndexes = false;
   bWithZeroDeltas = false;
+  bDontIgnoreOrder = false;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
     if (!strcmp(argv[curr_arg],"-indexes"))
       bIndexes = true;
     else if (!strcmp(argv[curr_arg],"-with_zero_deltas"))
       bWithZeroDeltas = true;
+    else if (!strcmp(argv[curr_arg],"-dont_ignore_order"))
+      bDontIgnoreOrder = true;
     else
       break;
   }
@@ -44,22 +48,40 @@ int main(int argc,char **argv)
   for (n = 0; n < 2; n++)
     card_string[n][2] = 0;
 
-  for (o = 0; o < POKER_52_2_PERMUTATIONS; o++) {
-    get_permutation_instance_two(
-      NUM_CARDS_IN_DECK,
-      &m,&n,o);
+  if (!bDontIgnoreOrder) {
+    for (o = 0; o < POKER_52_2_PERMUTATIONS; o++) {
+      get_permutation_instance_two(
+        NUM_CARDS_IN_DECK,
+        &m,&n,o);
 
-    if (!bIndexes) {
-      card_string_from_card_value(m,card_string[0]);
-      card_string_from_card_value(n,card_string[1]);
+      if (!bIndexes) {
+        card_string_from_card_value(m,card_string[0]);
+        card_string_from_card_value(n,card_string[1]);
 
-      if (rank_of(m) >= rank_of(n))
-        printf(fmt[bWithZeroDeltas],card_string[0],card_string[1]);
+        if (rank_of(m) >= rank_of(n))
+          printf(fmt[bWithZeroDeltas],card_string[0],card_string[1]);
+        else
+          printf(fmt[bWithZeroDeltas],card_string[1],card_string[0]);
+      }
       else
-        printf(fmt[bWithZeroDeltas],card_string[1],card_string[0]);
+        printf("%02d %02d\n",m,n);
     }
-    else
-      printf("%02d %02d\n",m,n);
+  }
+  else {
+    for (m = 0; m < NUM_CARDS_IN_DECK; m++) {
+      for (n = 0; n < NUM_CARDS_IN_DECK; n++) {
+        if (m != n) {
+          if (!bIndexes) {
+            card_string_from_card_value(m,card_string[0]);
+            card_string_from_card_value(n,card_string[1]);
+
+            printf(fmt[bWithZeroDeltas],card_string[0],card_string[1]);
+          }
+          else
+            printf("%02d %02d\n",m,n);
+        }
+      }
+    }
   }
 
   return 0;
