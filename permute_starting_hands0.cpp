ea7da1393ee74f40ac7954266800aa3ec18067e9
Aidan Lloyd <lloyd.aidan@gmail.com>
2016-03-28 22:19:49 -0400

    add -print_offset and -unique_first_cards flags

--- permute_starting_hands0.cpp
+++ permute_starting_hands0.cpp
@@ -8,7 +8,8 @@ using namespace std;
 #include "poker_hand.h"
 
 static char usage[] =
-"usage: permute_starting_hands0 (-card_strings) (-no_sort)\n";
+"usage: permute_starting_hands0 (-card_strings) (-no_sort) (-print_offset)\n"
+"  (-unique_first_cards)\n";
 
 int main(int argc,char **argv)
 {
@@ -17,23 +18,32 @@ int main(int argc,char **argv)
   int curr_arg;
   bool bCardStrings;
   bool bNoSort;
+  bool bPrintOffset;
+  bool bUniqueFirstCards;
   int cards[NUM_HOLE_CARDS_IN_HOLDEM_HAND];
   int ordered_cards[NUM_HOLE_CARDS_IN_HOLDEM_HAND];
   char card_string[3];
+  int prev_first_card;
 
-  if (argc > 3) {
+  if (argc > 5) {
     printf(usage);
     return 1;
   }
 
   bCardStrings = false;
   bNoSort = false;
+  bPrintOffset = false;
+  bUniqueFirstCards = false;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
     if (!strcmp(argv[curr_arg],"-card_strings"))
       bCardStrings = true;
     else if (!strcmp(argv[curr_arg],"-no_sort"))
       bNoSort = true;
+    else if (!strcmp(argv[curr_arg],"-print_offset"))
+      bPrintOffset = true;
+    else if (!strcmp(argv[curr_arg],"-unique_first_cards"))
+      bUniqueFirstCards = true;
     else
       break;
   }
@@ -54,6 +64,12 @@ int main(int argc,char **argv)
       ordered_cards[1] = cards[1];
     }
 
+    if (bUniqueFirstCards && (m > 0) && (cards[0] == prev_first_card))
+      continue;
+
+    if (bPrintOffset)
+      printf("%4d ",m);
+
     if (!bCardStrings)
       printf("%2d %2d\n",ordered_cards[0],ordered_cards[1]);
     else {
@@ -67,6 +83,8 @@ int main(int argc,char **argv)
           putchar(0x0a);
       }
     }
+
+    prev_first_card = cards[0];
   }
 
   return 0;
