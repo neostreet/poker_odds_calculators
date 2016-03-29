ea3d45942227e33cee18e588d72565f18c4607a2
Aidan Lloyd <lloyd.aidan@gmail.com>
2016-03-28 21:52:55 -0400

    add -card_strings and -no_sort flags

--- permute_starting_hands0.cpp
+++ permute_starting_hands0.cpp
@@ -7,13 +7,36 @@ using namespace std;
 #define MAIN_MODULE
 #include "poker_hand.h"
 
+static char usage[] =
+"usage: permute_starting_hands0 (-card_strings) (-no_sort)\n";
+
 int main(int argc,char **argv)
 {
   int m;
   int n;
+  int curr_arg;
+  bool bCardStrings;
+  bool bNoSort;
   int cards[NUM_HOLE_CARDS_IN_HOLDEM_HAND];
   int ordered_cards[NUM_HOLE_CARDS_IN_HOLDEM_HAND];
   char card_string[3];
+
+  if (argc > 3) {
+    printf(usage);
+    return 1;
+  }
+
+  bCardStrings = false;
+  bNoSort = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-card_strings"))
+      bCardStrings = true;
+    else if (!strcmp(argv[curr_arg],"-no_sort"))
+      bNoSort = true;
+    else
+      break;
+  }
 
   card_string[2] = 0;
 
@@ -22,7 +45,7 @@ int main(int argc,char **argv)
       NUM_CARDS_IN_DECK,
       &cards[0],&cards[1],m);
 
-    if (cards[0] < cards[1]) {
+    if (!bNoSort && (cards[0] < cards[1])) {
       ordered_cards[0] = cards[1];
       ordered_cards[1] = cards[0];
     }
@@ -31,7 +54,7 @@ int main(int argc,char **argv)
       ordered_cards[1] = cards[1];
     }
 
-    if (argc == 1)
+    if (!bCardStrings)
       printf("%2d %2d\n",ordered_cards[0],ordered_cards[1]);
     else {
       for (n = 0; n < NUM_HOLE_CARDS_IN_HOLDEM_HAND; n++) {
