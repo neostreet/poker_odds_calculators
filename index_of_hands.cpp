b5039ed9e6dce56450ff0cd2cb65d1c33d09a063
Aidan Lloyd <lloyd.aidan@gmail.com>
2016-03-31 21:05:10 -0400

    add -coalesce flag

--- index_of_hands.cpp
+++ index_of_hands.cpp
@@ -12,17 +12,19 @@ using namespace std;
 #define NUM_NONSUITED_NONPAIRS NUM_SUITED_NONPAIRS
 
 static char usage[] =
-"usage: index_of_hands (-card_strings)\n";
+"usage: index_of_hands (-coalesce) (-card_strings)\n";
 
 static char bad_rank[] = "bad rank in card%d: %s\n";
 static char bad_suit[] = "bad suit in card%d: %s\n";
 
-static int index_of_hand(int rank_ix1,int suit_ix1,int rank_ix2,int suit_ix2);
+static int index_of_hand(int *cards);
+static int coalesced_index_of_hand(int rank_ix1,int suit_ix1,int rank_ix2,int suit_ix2);
 
 int main(int argc,char **argv)
 {
   int n;
   int curr_arg;
+  bool bCoalesce;
   bool bCardStrings;
   int cards[NUM_HOLE_CARDS_IN_HOLDEM_HAND];
   int rank_ix[2];
@@ -30,15 +32,18 @@ int main(int argc,char **argv)
   int ix;
   char card_string[3];
 
-  if (argc > 2) {
+  if (argc > 3) {
     printf(usage);
     return 1;
   }
 
+  bCoalesce = false;
   bCardStrings = false;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
-    if (!strcmp(argv[curr_arg],"-card_strings"))
+    if (!strcmp(argv[curr_arg],"-coalesce"))
+      bCoalesce = true;
+    else if (!strcmp(argv[curr_arg],"-card_strings"))
       bCardStrings = true;
     else
       break;
@@ -47,19 +52,26 @@ int main(int argc,char **argv)
   card_string[2] = 0;
 
   for (cards[0] = 0; cards[0] < NUM_CARDS_IN_DECK - 1; cards[0]++) {
-    rank_ix[0] = rank_of(cards[0]);
-    suit_ix[0] = suit_of(cards[0]);
+    if (bCoalesce) {
+      rank_ix[0] = rank_of(cards[0]);
+      suit_ix[0] = suit_of(cards[0]);
+    }
 
     for (cards[1] = cards[0] + 1; cards[1] < NUM_CARDS_IN_DECK; cards[1]++) {
-      rank_ix[1] = rank_of(cards[1]);
-      suit_ix[1] = suit_of(cards[1]);
+      if (!bCoalesce) {
+        ix = index_of_hand(cards);
+        printf("%4d ",ix);
+      }
+      else {
+        rank_ix[1] = rank_of(cards[1]);
+        suit_ix[1] = suit_of(cards[1]);
 
-      ix = index_of_hand(rank_ix[0],suit_ix[0],rank_ix[1],suit_ix[1]);
-
-      printf("%d ",ix);
+        ix = coalesced_index_of_hand(rank_ix[0],suit_ix[0],rank_ix[1],suit_ix[1]);
+        printf("%3d ",ix);
+      }
 
       if (!bCardStrings)
-        printf("%d %d\n",cards[0],cards[1]);
+        printf("%2d %2d\n",cards[0],cards[1]);
       else {
         for (n = 0; n < NUM_HOLE_CARDS_IN_HOLDEM_HAND; n++) {
           card_string_from_card_value(cards[n],card_string);
@@ -77,7 +89,26 @@ int main(int argc,char **argv)
   return 0;
 }
 
-static int index_of_hand(int rank_ix1,int suit_ix1,int rank_ix2,int suit_ix2)
+static int index_of_hand(int *cards)
+{
+  int n;
+  int index;
+  int num_other_cards;
+
+  index = 0;
+  num_other_cards = NUM_CARDS_IN_DECK - 1;
+
+  for (n = 0; n < cards[0]; n++) {
+    index += num_other_cards;
+    num_other_cards--;
+  }
+
+  index += cards[1] - cards[0] - 1;
+
+  return index;
+}
+
+static int coalesced_index_of_hand(int rank_ix1,int suit_ix1,int rank_ix2,int suit_ix2)
 {
   int n;
   int work;
