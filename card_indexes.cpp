08704d22552c34bd97d607318f7d9f425d5c5fd3
Aidan Lloyd <lloyd.aidan@gmail.com>
2016-08-20 18:21:40 -0400

    add -sort flag

--- card_indexes.cpp
+++ card_indexes.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 using namespace std;
 
@@ -10,11 +11,12 @@ using namespace std;
 static char line[MAX_LINE_LEN];
 
 static char usage[] =
-"usage: card_indexes filename num_cards\n";
+"usage: card_indexes (-sort) filename num_cards\n";
 static char couldnt_open[] = "couldn't open %s\n";
 static char parse_error[] = "couldn't parse line %d, card %d: %d\n";
 
 static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+int compare(const void *elem1,const void *elem2);
 
 #define MAX_CARDS NUM_CARDS_IN_DECK
 
@@ -22,6 +24,8 @@ int main(int argc,char **argv)
 {
   int m;
   int n;
+  int curr_arg;
+  bool bSort;
   FILE *fptr;
   int line_no;
   int line_len;
@@ -29,17 +33,31 @@ int main(int argc,char **argv)
   int retval;
   int cards[MAX_CARDS];
 
-  if (argc != 3) {
+  if ((argc < 3) || (argc > 4)) {
     cout << usage << endl;
     return 1;
   }
 
-  if ((fptr = fopen(argv[1],"r")) == NULL) {
-    printf(couldnt_open,argv[1]);
+  bSort = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-sort"))
+      bSort = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 2) {
+    cout << usage << endl;
     return 2;
   }
 
-  sscanf(argv[2],"%d",&num_cards);
+  if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 3;
+  }
+
+  sscanf(argv[curr_arg+1],"%d",&num_cards);
 
   line_no = 0;
 
@@ -62,7 +80,7 @@ int main(int argc,char **argv)
 
     if (m == line_len) {
       printf(parse_error,line_no,-1,1);
-      return 3;
+      return 4;
     }
 
     for (n = 0; n < num_cards; n++) {
@@ -70,7 +88,7 @@ int main(int argc,char **argv)
 
       if (retval) {
         printf(parse_error,line_no,n,2);
-        return 4;
+        return 5;
       }
 
       m += 2;
@@ -85,13 +103,16 @@ int main(int argc,char **argv)
 
         if (m == line_len) {
           printf(parse_error,line_no,n,3);
-          return 5;
+          return 6;
         }
       }
     }
   }
 
   fclose(fptr);
+
+  if (bSort)
+    qsort(cards,num_cards,sizeof (int),compare);
 
   for (n = 0; n < num_cards; n++) {
     printf("%d",cards[n]);
@@ -127,4 +148,15 @@ static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
 
   line[local_line_len] = 0;
   *line_len = local_line_len;
+}
+
+int compare(const void *elem1,const void *elem2)
+{
+  int int1;
+  int int2;
+
+  int1 = *(int *)elem1;
+  int2 = *(int *)elem2;
+
+  return int1 - int2;
 }
