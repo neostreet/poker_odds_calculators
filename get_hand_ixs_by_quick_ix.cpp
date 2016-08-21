2ebf00b60bf28dcaf51d3b22d2ceb28bfb442e7c
Aidan Lloyd <lloyd.aidan@gmail.com>
2016-08-21 13:03:49 -0400

    add -verbose flag

--- get_hand_ixs_by_quick_ix.cpp
+++ get_hand_ixs_by_quick_ix.cpp
@@ -10,34 +10,65 @@ using namespace std;
 static char line[MAX_LINE_LEN];
 
 static char usage[] =
-"usage: get_hand_ixs_by_quick_ix hands_and_types_filename quick_ix";
+"usage: get_hand_ixs_by_quick_ix (-verbose) hands_and_types_filename quick_ix";
 static char couldnt_open[] = "couldn't open %s\n";
 static char parse_error[] = "couldn't parse line %d, card %d: %d\n";
 
 int main(int argc,char **argv)
 {
+  int m;
   int n;
+  int curr_arg;
+  bool bVerbose;
   int retval;
   struct hand_and_type *hands_and_types;
   int quick_ix;
 
-  if (argc != 3) {
+  if ((argc < 3) || (argc > 4)) {
     printf(usage);
     return 1;
   }
 
-  retval = read_hands_and_types(argv[1],&hands_and_types);
+  bVerbose = false;
 
-  if (retval) {
-    printf("read_hands_and_types() failed: %d\n",retval);
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-verbose"))
+      bVerbose = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 2) {
+    printf(usage);
     return 2;
   }
 
-  sscanf(argv[2],"%d",&quick_ix);
+  retval = read_hands_and_types(argv[curr_arg],&hands_and_types);
+
+  if (retval) {
+    printf("read_hands_and_types() failed: %d\n",retval);
+    return 3;
+  }
+
+  sscanf(argv[curr_arg+1],"%d",&quick_ix);
 
   for (n = 0; n < POKER_52_5_PERMUTATIONS; n++) {
-    if (hands_and_types[n].quick_ix == quick_ix)
-      printf("%d\n",n);
+    if (hands_and_types[n].quick_ix == quick_ix) {
+      if (!bVerbose)
+        printf("%d\n",n);
+      else {
+        printf("%d ",n);
+
+        for (m = 0; m < NUM_CARDS_IN_HAND; m++) {
+          printf("%d",hands_and_types[n].cards[m]);
+
+          if (m < NUM_CARDS_IN_HAND - 1)
+            putchar(' ');
+          else
+            putchar(0x0a);
+        }
+      }
+    }
   }
 
   return 0;
