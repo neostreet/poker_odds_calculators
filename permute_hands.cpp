86326b64944da697fab49ea6f438cf2c0878f86e
Aidan Lloyd <lloyd.aidan@gmail.com>
2018-05-22 21:44:49 -0400

    sort hand types by count

--- permute_hands.cpp
+++ permute_hands.cpp
@@ -11,6 +11,10 @@ static char usage[] = "usage: permute_hands (-verbose)\n";
 
 int card_values[NUM_CARDS_IN_DECK];
 
+static int hand_counts[NUM_HAND_TYPES];
+
+int compare(const void *elem1,const void *elem2);
+
 int main(int argc,char **argv)
 {
   int curr_arg;
@@ -22,10 +26,10 @@ int main(int argc,char **argv)
   int q;
   int r;
   PokerHand hand(NUM_CARDS_IN_DECK);
-  int hand_counts[NUM_HAND_TYPES];
   double pct;
   time_t start_time;
   time_t end_time;
+  int hand_type_ixs[NUM_HAND_TYPES];
 
   if (argc > 2) {
     printf(usage);
@@ -51,8 +55,10 @@ int main(int argc,char **argv)
 
   time(&start_time);
 
-  for (n = 0; n < NUM_HAND_TYPES; n++)
+  for (n = 0; n < NUM_HAND_TYPES; n++) {
     hand_counts[n] = 0;
+    hand_type_ixs[n] = n;
+  }
 
   for (r = 0; r < POKER_52_5_PERMUTATIONS; r++) {
     get_permutation_instance_five(
@@ -72,9 +78,11 @@ int main(int argc,char **argv)
 
   time(&end_time);
 
-  for (n = NUM_HAND_TYPES - 1; (n >= 0); n--) {
-    pct = (double)hand_counts[n] / (double)POKER_52_5_PERMUTATIONS;
-    printf("%s %9d %9.6lf\n",hand_type_abbrevs[n],hand_counts[n],pct);
+  qsort(hand_type_ixs,NUM_HAND_TYPES,sizeof (int),compare);
+
+  for (n = 0; n < NUM_HAND_TYPES; n++) {
+    pct = (double)hand_counts[hand_type_ixs[n]] / (double)POKER_52_5_PERMUTATIONS;
+    printf("%s %9d %9.6lf\n",hand_type_abbrevs[hand_type_ixs[n]],hand_counts[hand_type_ixs[n]],pct);
   }
 
   printf("============\n");
@@ -83,4 +91,15 @@ int main(int argc,char **argv)
   printf("\ncomputation time: %d seconds\n",end_time - start_time);
 
   return 0;
+}
+
+int compare(const void *elem1,const void *elem2)
+{
+  int int1;
+  int int2;
+
+  int1 = *(int *)elem1;
+  int2 = *(int *)elem2;
+
+  return hand_counts[int1] - hand_counts[int2];
 }
