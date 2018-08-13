32d1ff1e2143ba57ae0c899879e472334c41db5f
Aidan Lloyd <lloyd.aidan@gmail.com>
2018-08-12 20:40:08 -0400

    add -debug flag

--- time_old_52_2_index_of_hand.cpp
+++ time_old_52_2_index_of_hand.cpp
@@ -9,7 +9,7 @@ using namespace std;
 #include "poker_hand.h"
 
 static char usage[] =
-"usage: time_old_52_2_index_of_hand loop_count\n";
+"usage: time_old_52_2_index_of_hand (-debug) loop_count\n";
 
 static int index_of_hand(int *cards);
 
@@ -17,6 +17,8 @@ int main(int argc,char **argv)
 {
   int m;
   int n;
+  int curr_arg;
+  bool bDebug;
   int loop_count;
   int cards[NUM_HOLE_CARDS_IN_HOLDEM_HAND];
   time_t start_sec;
@@ -24,12 +26,26 @@ int main(int argc,char **argv)
   int ix;
   int bad_count;
 
-  if (argc != 2) {
+  if ((argc < 2) || (argc > 3)) {
     printf(usage);
     return 1;
   }
 
-  sscanf(argv[1],"%d",&loop_count);
+  bDebug = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-debug"))
+      bDebug =  true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  sscanf(argv[curr_arg],"%d",&loop_count);
   time(&start_sec);
 
   for (n = 0; n < loop_count; n++) {
@@ -42,14 +58,17 @@ int main(int argc,char **argv)
 
       ix = index_of_hand(cards);
 
-      if (ix != m) {
-        printf("bad index for %d: %d (%d %d)\n",m,ix,cards[0],cards[1]);
-        bad_count++;
-        // return 2;
+      if (bDebug) {
+        if (ix != m) {
+          printf("bad index for %d: %d (%d %d)\n",m,ix,cards[0],cards[1]);
+          bad_count++;
+          // return 2;
+        }
       }
     }
 
-    printf("\nbad_count = %d\n",bad_count);
+    if (bDebug)
+      printf("\nbad_count = %d\n",bad_count);
   }
 
   time(&stop_sec);
