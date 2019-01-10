1cf44b208d9a3a9abd02a78ca8e58ca4e65849ff
Aidan Lloyd <lloyd.aidan@gmail.com>
2019-01-09 21:25:57 -0500

    remove reliance on POKER_52_3_PERMUTATIONS

--- permute_flops.cpp
+++ permute_flops.cpp
@@ -24,25 +24,30 @@ int main(int argc,char **argv)
   for (n = 0; n < NUM_FLOP_TYPES; n++)
     flop_counts[n] = 0;
 
-  for (p = 0; p < POKER_52_3_PERMUTATIONS; p++) {
+  for (p = 0; ; ) {
     get_permutation_instance_three(
       NUM_CARDS_IN_DECK,
       &m,&n,&o,p);
 
+    p++;
+
     flop.NewCards(m,n,o);
     flop.Evaluate();
     flop_counts[flop.GetFlopType()]++;
+
+    if (m == NUM_CARDS_IN_DECK - 3)
+      break;
   }
 
   time(&end_time);
 
   for (n = NUM_FLOP_TYPES - 1; (n >= 0); n--) {
-    pct = (double)flop_counts[n] / (double)POKER_52_3_PERMUTATIONS;
+    pct = (double)flop_counts[n] / (double)p;
     printf("%s %5d %9.6lf\n",flop_type_abbrevs[n],flop_counts[n],pct);
   }
 
   printf("=========\n");
-  printf("    %5d\n",POKER_52_3_PERMUTATIONS);
+  printf("    %5d\n",p);
 
   printf("\ncomputation time: %d seconds\n",end_time - start_time);
 
