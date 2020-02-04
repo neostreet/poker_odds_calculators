ef4ad15520e619e707c3859e3e45e1422188f7b7
Aidan Lloyd <lloyd.aidan@gmail.com>
2020-02-04 13:59:50 -0500

    revert permute_hands0.cpp

--- permute_hands0.cpp
+++ permute_hands0.cpp
@@ -3,109 +3,99 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#ifdef WIN32
+#include <io.h>
+#else
+#include <unistd.h>
+#ifdef FREEBSD
+#define O_BINARY 0
+#endif
+#endif
 using namespace std;
 
 #define MAIN_MODULE
 #include "poker_hand.h"
 
-static char usage[] = "usage: permute_hands0 (-verbose)\n";
-
-int card_values[NUM_CARDS_IN_DECK];
-
-static int hand_counts[NUM_HAND_TYPES];
-
-int compare(const void *elem1,const void *elem2);
+static char usage[] =
+"usage: permute_hands0 (-binfilefile)\n";
+static char couldnt_open[] = "couldn't open %s\n";
 
 int main(int argc,char **argv)
 {
-  int curr_arg;
-  bool bVerbose;
   int m;
   int n;
-  int o;
-  int p;
-  int q;
-  int r;
+  int curr_arg;
+  bool bBinFile;
+  char *binfile_name;
+  int cards[NUM_CARDS_IN_HAND];
+  char card_string[3];
   PokerHand hand(NUM_CARDS_IN_DECK);
-  double pct;
-  time_t start_time;
-  time_t end_time;
-  int hand_type_ixs[NUM_HAND_TYPES];
-  int total;
+  struct hand_and_type *hands_and_types;
+  int fhndl;
 
   if (argc > 2) {
     printf(usage);
     return 1;
   }
 
-  bVerbose = false;
+  bBinFile = false;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
-    if (!strcmp(argv[curr_arg],"-verbose"))
-      bVerbose = true;
+    if (!strncmp(argv[curr_arg],"-binfile",8)) {
+      bBinFile = true;
+      binfile_name = &argv[curr_arg][8];
+    }
     else
       break;
   }
 
-  if (argc - curr_arg != 0) {
-    printf(usage);
-    return 2;
+  if (bBinFile) {
+    hands_and_types = (struct hand_and_type *)malloc(sizeof (struct hand_and_type) * POKER_52_5_PERMUTATIONS);
+
+    if (hands_and_types == NULL) {
+      printf("malloc of hands_and_types failed\n");
+      return 3;
+    }
   }
 
-  for (n = 0; n < NUM_CARDS_IN_DECK; n++)
-    card_values[n] = n;
+  card_string[2] = 0;
 
-  time(&start_time);
-
-  for (n = 0; n < NUM_HAND_TYPES; n++) {
-    hand_counts[n] = 0;
-    hand_type_ixs[n] = n;
-  }
-
-  for (r = 0; r < POKER_52_5_PERMUTATIONS; r++) {
+  for (m = 0; m < POKER_52_5_PERMUTATIONS; m++) {
     get_permutation_instance_five(
       NUM_CARDS_IN_DECK,
-      &m,&n,&o,&p,&q,r);
+      &cards[0],&cards[1],&cards[2],&cards[3],&cards[4],m);
 
-    hand.NewCards(card_values[m],card_values[n],card_values[o],card_values[p],card_values[q]);
-    hand.Evaluate0(hand_counts);
+    hand.NewCards(cards[0],cards[1],cards[2],cards[3],cards[4]);
+    hand.Evaluate();
 
-    if (bVerbose)
+    if (bBinFile) {
+      for (n = 0; n < NUM_CARDS_IN_HAND; n++)
+        hands_and_types[m].cards[n] = (char)cards[n];
+
+      hands_and_types[m].hand_type = (char)hand.GetHandType();
+      hands_and_types[m].hand_ix = m;
+    }
+    else
       cout << hand << endl;
   }
 
-  if (bVerbose)
-    cout << endl;
+  if (bBinFile) {
+    if ((fhndl = open(binfile_name,
+      O_CREAT | O_EXCL | O_BINARY | O_WRONLY,
+      S_IREAD | S_IWRITE)) == -1) {
 
-  time(&end_time);
+      printf(couldnt_open,binfile_name);
+      return 4;
+    }
 
-  qsort(hand_type_ixs,NUM_HAND_TYPES,sizeof (int),compare);
+    write(fhndl,hands_and_types,sizeof (struct hand_and_type) * POKER_52_5_PERMUTATIONS);
+    close(fhndl);
 
-  total = 0;
-
-  for (n = 0; n < NUM_HAND_TYPES; n++)
-    total += hand_counts[n];
-
-  for (n = 0; n < NUM_HAND_TYPES; n++) {
-    pct = (double)hand_counts[hand_type_ixs[n]] / (double)total;
-    printf("%s %9d %9.6lf\n",hand_type_abbrevs[hand_type_ixs[n]],hand_counts[hand_type_ixs[n]],pct);
+    free(hands_and_types);
   }
 
-  printf("============\n");
-  printf("   %9d\n",total);
-
-  printf("\ncomputation time: %d seconds\n",end_time - start_time);
-
   return 0;
-}
-
-int compare(const void *elem1,const void *elem2)
-{
-  int int1;
-  int int2;
-
-  int1 = *(int *)elem1;
-  int2 = *(int *)elem2;
-
-  return hand_counts[int1] - hand_counts[int2];
 }
