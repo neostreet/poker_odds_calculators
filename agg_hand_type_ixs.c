284dcc6534fbf0b85229671a519cfc7fd47752fd
neostreet <lloyd.aidan@gmail.com>
2023-10-08 19:03:05 -0400

    add -hand_count flag

--- agg_hand_type_ixs.c
+++ agg_hand_type_ixs.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
 #define MAX_LINE_LEN 1024
 static char line[MAX_LINE_LEN];
 
-static char usage[] = "usage: agg_hand_type_ixs filename\n";
+static char usage[] = "usage: agg_hand_type_ixs (-hand_count) filename\n";
 static char couldnt_open[] = "couldn't open %s\n";
 
 static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
 
 int main(int argc,char **argv)
 {
+  int curr_arg;
+  bool bHandCount;
   FILE *fptr;
   int line_len;
   int line_no;
@@ -19,14 +22,28 @@ int main(int argc,char **argv)
   int total;
   int grand_total;
 
-  if (argc != 2) {
+  if ((argc < 2) || (argc > 3)) {
     printf(usage);
     return 1;
   }
 
-  if ((fptr = fopen(argv[1],"r")) == NULL) {
-    printf(couldnt_open,argv[1]);
+  bHandCount = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-hand_count"))
+      bHandCount = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
     return 2;
+  }
+
+  if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 3;
   }
 
   line_no = 0;
@@ -40,17 +57,31 @@ int main(int argc,char **argv)
 
     line_no++;
 
-    sscanf(line,"%d %d",&hand_type_ix,&hand_count);
+    if (bHandCount)
+      sscanf(line,"%d %d",&hand_type_ix,&hand_count);
+    else
+      sscanf(line,"%d",&hand_type_ix);
 
     if (line_no == 1) {
-      total = hand_count;
+      if (bHandCount)
+        total = hand_count;
+      else
+        total = 1;
     }
-    else if (hand_type_ix == prev_hand_type_ix)
-      total += hand_count;
+    else if (hand_type_ix == prev_hand_type_ix) {
+      if (bHandCount)
+        total += hand_count;
+      else
+        total++;
+    }
     else {
       printf("%d %7d\n",prev_hand_type_ix,total);
       grand_total += total;
-      total = hand_count;
+
+      if (bHandCount)
+        total = hand_count;
+      else
+        total = 1;
     }
 
     prev_hand_type_ix = hand_type_ix;
