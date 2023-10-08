8ea7198c6e2d9c055c88d45c69e4224eaad8b923
neostreet <lloyd.aidan@gmail.com>
2023-10-08 12:46:09 -0400

    add -sort flag

--- all_hands.cpp
+++ all_hands.cpp
@@ -10,10 +10,14 @@ using namespace std;
 #define MAX_LINE_LEN 1024
 static char line[MAX_LINE_LEN];
 
+static struct hand_and_type *hands_and_types;
+
 static char usage[] =
-"usage: all_hands (-countcount) hands_and_types_filename\n";
+"usage: all_hands (-countcount) (-sort) hands_and_types_filename\n";
 
 static char couldnt_open[] = "couldn't open %s\n";
+
+int elem_compare(const void *elem1,const void *elem2);
 
 int main(int argc,char **argv)
 {
@@ -21,16 +25,18 @@ int main(int argc,char **argv)
   int n;
   int curr_arg;
   int count;
+  bool bSort;
   int retval;
-  struct hand_and_type *hands_and_types;
   char card_string[3];
+  int *ixs;
 
-  if ((argc < 2) || (argc > 3)) {
+  if ((argc < 2) || (argc > 4)) {
     printf(usage);
     return 1;
   }
 
   count = POKER_52_5_PERMUTATIONS;
+  bSort = false;
 
   for (curr_arg = 1; curr_arg < argc; curr_arg++) {
     if (!strncmp(argv[curr_arg],"-count",6)) {
@@ -39,6 +45,8 @@ int main(int argc,char **argv)
       if (count > POKER_52_5_PERMUTATIONS)
         count = POKER_52_5_PERMUTATIONS;
     }
+    else if (!strcmp(argv[curr_arg],"-sort"))
+      bSort = true;
     else
       break;
   }
@@ -57,18 +65,44 @@ int main(int argc,char **argv)
 
   card_string[2] = 0;
 
+  ixs = (int *)malloc(count * sizeof (int));
+
+  if (ixs == NULL) {
+    printf("couldn't malloc %d ints for ixs\n",count);
+    return 4;
+  }
+
+  for (n = 0; n < count; n++)
+    ixs[n] = n;
+
+  if (bSort)
+    qsort(ixs,count,sizeof (int),elem_compare);
+
   for (n = 0; n < count; n++) {
-    printf("%4d ",hands_and_types[n].quick_ix);
+    printf("%4d ",hands_and_types[ixs[n]].quick_ix);
 
     for (m = 0; m < NUM_CARDS_IN_HAND; m++) {
       card_string_from_card_value(
-        hands_and_types[n].cards[m],
+        hands_and_types[ixs[n]].cards[m],
         card_string);
       printf("%s ",card_string);
     }
 
-    printf("%s\n",plain_hand_types[hands_and_types[n].hand_type]);
+    printf("%s\n",plain_hand_types[hands_and_types[ixs[n]].hand_type]);
   }
 
+  free(ixs);
+
   return 0;
+}
+
+int elem_compare(const void *elem1,const void *elem2)
+{
+  int ix1;
+  int ix2;
+
+  ix1 = *(int *)elem1;
+  ix2 = *(int *)elem2;
+
+  return hands_and_types[ix1].quick_ix - hands_and_types[ix2].quick_ix;
 }
