423234b30fcf77dad119b78493dbc8f684945ec5
neostreet <lloyd.aidan@gmail.com>
2024-10-09 19:48:57 -0400

    add is_premium_hand.cpp

---
+++ is_premium_hand.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+using namespace std;
+
+#define MAIN_MODULE
+#include "poker_hand.h"
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static char usage[] = "usage: is_premium_hand filename";
+static char couldnt_open[] = "couldn't open %s\n";
+static char parse_error[] = "couldn't parse line %d, card %d: %d\n";
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+
+int main(int argc,char **argv)
+{
+  FILE *fptr;
+  int line_no;
+  int line_len;
+
+  if (argc != 2) {
+    cout << usage << endl;
+    return 1;
+  }
+
+  if ((fptr = fopen(argv[1],"r")) == NULL) {
+    printf(couldnt_open,argv[1]);
+    return 2;
+  }
+
+  line_no = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+    if (feof(fptr))
+      break;
+
+    if (is_premium_hand(line))
+      printf("1 %s\n",line);
+    else
+      printf("0 %s\n",line);
+  }
+
+  fclose(fptr);
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
