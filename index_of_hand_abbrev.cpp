019f781375b89d62f6a7180a62f077d49ddb445a
neostreet <lloyd.aidan@gmail.com>
2025-01-31 16:09:49 -0500

    add index_of_hand_abbrev.cpp

--- index_of_hand_abbrev.cpp
+++ index_of_hand_abbrev.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <time.h>
 using namespace std;
@@ -8,24 +7,76 @@ using namespace std;
 #define MAIN_MODULE
 #include "poker_hand.h"
 
-static char usage[] = "usage: index_of_hand_abbrev hand_abbrev\n";
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static char usage[] = "usage: index_of_hand_abbrev filename";
+static char couldnt_open[] = "couldn't open %s\n";
+static char parse_error[] = "couldn't parse line %d, card %d: %d\n";
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
 
 int main(int argc,char **argv)
 {
+  FILE *fptr;
+  int line_no;
+  int line_len;
   int retval;
-  int ix;
+  int abbrev_ix;
 
   if (argc != 2) {
-    printf(usage);
+    cout << usage << endl;
     return 1;
   }
 
-  retval = index_of_hand_abbrev(argv[1],&ix);
+  if ((fptr = fopen(argv[1],"r")) == NULL) {
+    printf(couldnt_open,argv[1]);
+    return 2;
+  }
 
-  if (retval)
-    printf("index_of_hand_abbrev() failed: %d\n",retval);
-  else
-    printf("%d %s\n",ix,argv[1]);
+  line_no = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+    if (feof(fptr))
+      break;
+
+    line_no++;
+
+    retval = index_of_hand_abbrev(line,&abbrev_ix);
+
+    if (!retval)
+      printf("%3d %s\n",abbrev_ix,line);
+    else
+      printf("index_of_hand_abbrev() failed on line %d: %d\n",line_no,retval);
+  }
+
+  fclose(fptr);
 
   return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
 }
