ee72eb2673de05487a15acbbd8ce51c76841f479
neostreet <lloyd.aidan@gmail.com>
2025-02-08 16:19:23 -0500

    add count_premium_hands2.cpp

---
+++ count_premium_hands2.cpp
@@ -0,0 +1,153 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+using namespace std;
+
+#define MAIN_MODULE
+#include "poker_hand.h"
+
+#define MAX_FILENAME_LEN 1024
+static char filename[MAX_FILENAME_LEN];
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static char usage[] = "usage: count_premium_hands2 (-hole_cards) filename\n";
+static char couldnt_open[] = "couldn't open %s\n";
+static char parse_error[] = "couldn't parse line %d, card %d: %d\n";
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+
+int main(int argc,char **argv)
+{
+  int curr_arg;
+  bool bHoleCards;
+  bool bPremiumHand;
+  FILE *fptr0;
+  int filename_len;
+  FILE *fptr;
+  int line_len;
+  int premium_ix;
+  int premium_hands;
+  int hands;
+  int total_premium_hands;
+  int total_hands;
+  char hole_cards[6];
+  char hole_cards_abbrev[4];
+  double dwork;
+
+  if ((argc < 2) || (argc > 3)) {
+    printf(usage);
+    return 1;
+  }
+
+  bHoleCards = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-hole_cards"))
+      bHoleCards = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  if ((fptr0 = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 3;
+  }
+
+  total_premium_hands = 0;
+  total_hands = 0;
+
+  if (bHoleCards) {
+    hole_cards[2] = ' ';
+    hole_cards[5] = 0;
+    hole_cards_abbrev[3] = 0;
+  }
+
+  for ( ; ; ) {
+    GetLine(fptr0,filename,&filename_len,MAX_FILENAME_LEN);
+
+    if (feof(fptr0))
+      break;
+
+    if ((fptr = fopen(filename,"r")) == NULL) {
+      printf(couldnt_open,filename);
+      return 4;
+    }
+
+    premium_hands = 0;
+    hands = 0;
+
+    for ( ; ; ) {
+      GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+      if (feof(fptr))
+        break;
+
+      hands++;
+
+      if (!bHoleCards)
+        bPremiumHand = is_premium_hand(line,&premium_ix);
+      else {
+        if ((line[2] != ' ') || (line[5] && (line[5] != ' ') && (line[5] != ','))) {
+          printf("invalid hole card delimiters in line %d\n",total_hands);
+          return 5;
+        }
+
+        hole_cards[0] = line[0];
+        hole_cards[1] = line[1];
+        hole_cards[3] = line[3];
+        hole_cards[4] = line[4];
+
+        get_abbrev(hole_cards,hole_cards_abbrev);
+
+        bPremiumHand = is_premium_hand(hole_cards_abbrev,&premium_ix);
+      }
+
+      if (bPremiumHand)
+        premium_hands++;
+    }
+
+    fclose(fptr);
+
+    dwork = (double)premium_hands / (double) hands * (double)100;
+    printf("%6d of %6d hands (%6.2lf) %s\n",premium_hands,hands,dwork,filename);
+
+    total_premium_hands += premium_hands;
+    total_hands += hands;
+  }
+
+  dwork = (double)total_premium_hands / (double) total_hands * (double)100;
+  printf("\n%6d of %6d hands (%6.2lf)\n",total_premium_hands,total_hands,dwork);
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
