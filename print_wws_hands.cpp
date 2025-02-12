406a4c7263a0e7be46f31b134583221e434f6bf2
neostreet <lloyd.aidan@gmail.com>
2025-02-11 19:02:42 -0500

    add print_ws_hands.cpp and print_ws_hands2.cpp

---
+++ print_wws_hands.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+using namespace std;
+
+#define MAIN_MODULE
+#include "poker_hand.h"
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static char usage[] = "usage: print_wws_hands (-verbose) filename\n";
+
+static char wws_str[] = ", wws";
+#define WWS_STR_LEN (sizeof (wws_str) - 1)
+
+static char couldnt_open[] = "couldn't open %s\n";
+static char parse_error[] = "couldn't parse line %d, card %d: %d\n";
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+static bool Contains(bool bCaseSens,char *line,int line_len,
+  char *string,int string_len,int *index);
+
+int main(int argc,char **argv)
+{
+  int curr_arg;
+  bool bVerbose;
+  FILE *fptr;
+  int line_len;
+  int hands;
+  int ix;
+
+  if ((argc < 2) || (argc > 3)) {
+    printf(usage);
+    return 1;
+  }
+
+  bVerbose = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-verbose"))
+      bVerbose = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  if ((fptr = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 4;
+  }
+
+  hands = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+    if (feof(fptr))
+      break;
+
+    hands++;
+
+    if (Contains(true,
+      line,line_len,
+      wws_str,WWS_STR_LEN,
+      &ix)) {
+
+      if (!bVerbose)
+        printf("%s\n",line);
+      else
+        printf("%s %s hand %d\n",line,argv[curr_arg],hands);
+    }
+  }
+
+  fclose(fptr);
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
+
+static bool Contains(bool bCaseSens,char *line,int line_len,
+  char *string,int string_len,int *index)
+{
+  int m;
+  int n;
+  int tries;
+  char chara;
+
+  tries = line_len - string_len + 1;
+
+  if (tries <= 0)
+    return false;
+
+  for (m = 0; m < tries; m++) {
+    for (n = 0; n < string_len; n++) {
+      chara = line[m + n];
+
+      if (!bCaseSens) {
+        if ((chara >= 'A') && (chara <= 'Z'))
+          chara += 'a' - 'A';
+      }
+
+      if (chara != string[n])
+        break;
+    }
+
+    if (n == string_len) {
+      *index = m;
+      return true;
+    }
+  }
+
+  return false;
+}
