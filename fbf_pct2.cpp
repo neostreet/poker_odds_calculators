bca31eafc489f1cbde329f2c348a8fd38f4bf28e
neostreet <lloyd.aidan@gmail.com>
2025-03-05 13:57:02 -0500

    add fbf_pct.cpp and fbf_pct2.cpp

---
+++ fbf_pct2.cpp
@@ -0,0 +1,233 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+using namespace std;
+
+#define MAIN_MODULE
+#include "poker_hand.h"
+
+#define MAX_FILENAME_LEN 1024
+static char filename[MAX_FILENAME_LEN];
+
+#define MAX_LINE_LEN 1024
+static char line[MAX_LINE_LEN];
+
+static char usage[] = "usage: fbf_pct2 (-debug) (-premium) (-not_premium) filename\n";
+
+static char fbf_str[] = ", fbf";
+#define FBF_STR_LEN (sizeof (fbf_str) - 1)
+
+static char couldnt_open[] = "couldn't open %s\n";
+static char parse_error[] = "couldn't parse line %d, card %d: %d\n";
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen);
+static bool Contains(bool bCaseSens,char *line,int line_len,
+  char *string,int string_len,int *index);
+
+int main(int argc,char **argv)
+{
+  int curr_arg;
+  bool bDebug;
+  bool bPremium;
+  bool bNotPremium;
+  FILE *fptr0;
+  int filename_len;
+  FILE *fptr;
+  int line_len;
+  int hands;
+  int fbf_count;
+  int total_hands;
+  int total_fbf_count;
+  int ix;
+  int premium_ix;
+  char hole_cards[6];
+  char hole_cards_abbrev[4];
+  double dwork;
+
+  if ((argc < 2) || (argc > 5)) {
+    printf(usage);
+    return 1;
+  }
+
+  bDebug = false;
+  bPremium = false;
+  bNotPremium = false;
+
+  for (curr_arg = 1; curr_arg < argc; curr_arg++) {
+    if (!strcmp(argv[curr_arg],"-debug"))
+      bDebug = true;
+    else if (!strcmp(argv[curr_arg],"-premium"))
+      bPremium = true;
+    else if (!strcmp(argv[curr_arg],"-not_premium"))
+      bNotPremium = true;
+    else
+      break;
+  }
+
+  if (argc - curr_arg != 1) {
+    printf(usage);
+    return 2;
+  }
+
+  if (bPremium && bNotPremium) {
+    printf("can't specify both -premium and -not_premium\n");
+    return 3;
+  }
+
+  if ((fptr0 = fopen(argv[curr_arg],"r")) == NULL) {
+    printf(couldnt_open,argv[curr_arg]);
+    return 4;
+  }
+
+  if (bPremium || bNotPremium) {
+    hole_cards[2] = ' ';
+    hole_cards[5] = 0;
+    hole_cards_abbrev[3] = 0;
+  }
+
+  total_hands = 0;
+  total_fbf_count = 0;
+
+  for ( ; ; ) {
+    GetLine(fptr0,filename,&filename_len,MAX_FILENAME_LEN);
+
+    if (feof(fptr0))
+      break;
+
+    if ((fptr = fopen(filename,"r")) == NULL) {
+      printf(couldnt_open,filename);
+      return 5;
+    }
+
+    hands = 0;
+    fbf_count = 0;
+
+    for ( ; ; ) {
+      GetLine(fptr,line,&line_len,MAX_LINE_LEN);
+
+      if (feof(fptr))
+        break;
+
+      hands++;
+
+      if (bPremium || bNotPremium) {
+        if ((line[2] != ' ') || (line[5] && (line[5] != ' ') && (line[5] != ','))) {
+          printf("invalid hole card delimiters in line %d\n",hands);
+          return 6;
+        }
+
+        if ((line[0] >= 'a') && (line[0] <= 'z'))
+          line[0] -= 'a' - 'A';
+
+        if ((line[3] >= 'a') && (line[3] <= 'z'))
+          line[3] -= 'a' - 'A';
+
+        hole_cards[0] = line[0];
+        hole_cards[1] = line[1];
+        hole_cards[3] = line[3];
+        hole_cards[4] = line[4];
+
+        get_abbrev(hole_cards,hole_cards_abbrev);
+
+        if (bPremium) {
+          if (!is_premium_hand(hole_cards_abbrev,&premium_ix))
+            continue;
+        }
+        else if (bNotPremium) {
+          if (is_premium_hand(hole_cards_abbrev,&premium_ix))
+            continue;
+        }
+      }
+
+      if (Contains(true,
+        line,line_len,
+        fbf_str,FBF_STR_LEN,
+        &ix)) {
+
+        fbf_count++;
+      }
+    }
+
+    fclose(fptr);
+
+    total_hands += hands;
+    total_fbf_count += fbf_count;
+
+    if (!hands)
+      dwork = (double)0;
+    else
+      dwork = (double)fbf_count / (double)hands * (double)100;
+
+    printf("%6.2lf%% (folded before flop on %d of %d hands) %s\n",dwork,fbf_count,hands,filename);
+  }
+
+  fclose(fptr0);
+
+  if (!total_hands)
+    dwork = (double)0;
+  else
+    dwork = (double)total_fbf_count / (double)total_hands * (double)100;
+
+  printf("\n%6.2lf%% (folded before flop on %d of %d total_hands)\n",dwork,total_fbf_count,total_hands);
+
+  return 0;
+}
+
+static void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
+{
+  int chara;
+  int local_line_len;
+
+  local_line_len = 0;
+
+  for ( ; ; ) {
+    chara = fgetc(fptr);
+
+    if (feof(fptr))
+      break;
+
+    if (chara == '\n')
+      break;
+
+    if (local_line_len < maxllen - 1)
+      line[local_line_len++] = (char)chara;
+  }
+
+  line[local_line_len] = 0;
+  *line_len = local_line_len;
+}
+
+static bool Contains(bool bCaseSens,char *line,int line_len,
+  char *string,int string_len,int *index)
+{
+  int m;
+  int n;
+  int tries;
+  char chara;
+
+  tries = line_len - string_len + 1;
+
+  if (tries <= 0)
+    return false;
+
+  for (m = 0; m < tries; m++) {
+    for (n = 0; n < string_len; n++) {
+      chara = line[m + n];
+
+      if (!bCaseSens) {
+        if ((chara >= 'A') && (chara <= 'Z'))
+          chara += 'a' - 'A';
+      }
+
+      if (chara != string[n])
+        break;
+    }
+
+    if (n == string_len) {
+      *index = m;
+      return true;
+    }
+  }
+
+  return false;
+}
